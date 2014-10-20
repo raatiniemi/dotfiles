178c636cdc13fc3e61f2136a7a1d41b258b12d91
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-20 04:33:35 +0200

    Added aliases for directory jumping, ssh, etc.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,4 +1,18 @@
 #!/bin/zsh
+# Jump back `n` directories at a time
+# # http://alias.sh/jump-back-n-directories-time
+alias ..='cd ..';
+alias ...='cd ../../';
+alias ....='cd ../../../';
+alias .....='cd ../../../../';
+alias ......='cd ../../../../../';
+
+# Gzip-enabled `curl`
+alias gurl='curl --compressed';
+
+# # Get week number
+alias week='date +%V';
+
 # Shortcuts for common git commands.
 alias gs="git status";
 alias gd="git diff";
@@ -7,3 +21,6 @@ alias ga="git add";
 alias gm="git mv";
 alias gr="git rm";
 alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+# Shortcuts for SSH operations
+alias sshx="ssh -XC -c blowfish-cbc,arcfour";
