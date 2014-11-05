fac466f04a800c2f476a907c7ee4666262ced062
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-11-05 04:59:22 +0100

    git: Moved git related aliases to separate file within the git directory.

---
+++ git/aliases.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+alias gs="git status";
+alias gd="git diff";
+alias gc="git commit";
+alias ga="git add";
+alias gm="git mv";
+alias gr="git rm";
+alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
