69ed9721a4e92f0fa94b72977a3e262a164c2558
Tobias Raatiniemi <raatiniemi@gmail.com>
2016-07-27 04:23:38 +0200

    remove unused git aliases

--- git/alias.zsh
+++ git/alias.zsh
@@ -4,6 +4,4 @@ alias gd="git diff";
 alias gdc="git diff --cached";
 alias gc="git commit";
 alias ga="git add";
-alias gm="git mv";
-alias gr="git rm";
 alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
